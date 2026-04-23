72a94c17e81bc7683ed7b593915f1717bf1ebce4
MrTomSoyer <107312791+MrTomSoyer@users.noreply.github.com>
2026-04-23 19:50:36 +0400

    Update main.py

--- main.py
+++ main.py
@@ -8,38 +8,49 @@ from selenium.webdriver.support import expected_conditions as EC
 def setup_driver():
     try:
         options = webdriver.ChromeOptions()
-        options.add_argument("--headless")
+        options.add_argument('--headless')
         options.page_load_strategy = 'eager'
         driver = webdriver.Chrome(options=options)
         return driver
     except WebDriverException as e:
-        print(f"Ошибка WebDriver: {e}")
-        print(f"Тип ошибки: {type(e).__name__}")
+        print(f'Ошибка WebDriver: {e}')
+        print(f'Тип ошибки: {type(e).__name__}')
         return False
 
 
-def search_in_post(driver, link_, word):
+def search_in_post(driver, link_, keywords):
+    main_window = driver.current_window_handle
+
     try:
-        driver.get(link_)
+        driver.execute_script("window.open(arguments[0]);", link_)
+        driver.switch_to.window(driver.window_handles[-1])
+
         wait = WebDriverWait(driver, 10)
         text_box = wait.until(
             EC.presence_of_element_located((
-                By.CSS_SELECTOR,
-                'div[xmlns="http://www.w3.org/1999/xhtml"]'
+                By.ID, 'post-content-body'
             ))
         )
-        return word in text_box.text.lower()
+
+        text = text_box.text.lower()
+        result = any(word in text for word in keywords)
+
+        return result
 
     except TimeoutException:
-        print("Таймаут при загрузке страницы с постом")
+        print('Таймаут при загрузке страницы с постом')
         return False
     except NoSuchElementException:
         print('Не найден элемент страницы')
         return False
     except WebDriverException as e:
-        print(f"Ошибка инициализации WebDriver: {e}")
-        print(f"Тип ошибки: {type(e).__name__}")
+        print(f'Ошибка инициализации WebDriver: {e}')
+        print(f'Тип ошибки: {type(e).__name__}')
         return False
+    finally:
+        if driver.current_window_handle != main_window:
+            driver.close()
+            driver.switch_to.window(main_window)
 
 
 def search_by_preview(keywords, is_search_in_post=False):
@@ -48,27 +59,22 @@ def search_by_preview(keywords, is_search_in_post=False):
         return
 
     try:
-        driver.get("https://habr.com/ru/articles")
+        driver.get('https://habr.com/ru/articles')
         wait = WebDriverWait(driver, 10)
 
-        wait.until(EC.presence_of_all_elements_located((By.TAG_NAME, "article")))
-        articles = driver.find_elements(by=By.TAG_NAME, value="article")
+        wait.until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, 'article')))
+        articles = driver.find_elements(By.CSS_SELECTOR, 'article')
 
         articles_data = []
         for element in articles:
-            if not element.is_displayed():
-                continue
+            date_tag = element.find_element(By.CSS_SELECTOR, 'time')
+            data = date_tag.get_attribute('datetime')[:10]
 
-            time_tag = element.find_element(by=By.TAG_NAME, value="time")
-            data = time_tag.get_attribute("datetime")[:10]
-
-            header_tag = element.find_element(By.TAG_NAME, "h2")
+            header_tag = element.find_element(By.CSS_SELECTOR, 'a.tm-title__link')
             title = header_tag.text
+            link = header_tag.get_attribute('href')
 
-            link_tag = header_tag.find_element(By.TAG_NAME, "a")
-            link = link_tag.get_attribute("href")
-
-            descriptions = element.find_elements(By.TAG_NAME, "p")
+            descriptions = element.find_elements(By.CSS_SELECTOR,'.article-formatted-body')
             content = '\n'.join([desc.text for desc in descriptions])
 
             articles_data.append({
@@ -80,29 +86,32 @@ def search_by_preview(keywords, is_search_in_post=False):
 
         for article_data in articles_data:
             all_text = article_data['title'].lower() + '\n' + article_data['content']
+            found_in_preview = any(word in all_text for word in keywords)
 
-            for word in keywords:
-                if word in all_text:
-                    print(f'{article_data['data']}\n{article_data['title']}\n{article_data['link']}\n-----')
-                    break
-                elif is_search_in_post and search_in_post(driver, article_data['link'], word):
-                    print(f'{article_data['data']}\n{article_data['title']}\n{article_data['link']}\n-----')
-                    break
+            if found_in_preview:
+                is_found = True
+            elif is_search_in_post:
+                is_found = search_in_post(driver, article_data['link'], keywords)
+            else:
+                is_found = False
+
+            if is_found:
+                print(f'<{article_data["data"]}> — <{article_data["title"]}> — <{article_data["link"]}>')
 
     except TimeoutException:
-        print("Таймаут при загрузке страницы с превью")
+        print('Таймаут при загрузке страницы с превью')
     except WebDriverException as e:
-        print(f"Ошибка WebDriver: {e}")
+        print(f'Ошибка WebDriver: {e}')
     except Exception as e:
-        print(f"Ошибка: {e}")
-        print(f"Тип ошибки: {type(e).__name__}")
+        print(f'Ошибка: {e}')
+        print(f'Тип ошибки: {type(e).__name__}')
     finally:
         if driver:
             driver.quit()
 
 KEYWORDS = ['дизайн', 'фото', 'web', 'python']
 
-if __name__ == "__main__":
-    search_by_preview(KEYWORDS)
-    # # Поиск внутри статьи
-    # search_by_preview(KEYWORDS, True)
+if __name__ == '__main__':
+    #search_by_preview(KEYWORDS)
+    # Поиск внутри статьи
+    search_by_preview(KEYWORDS, True)
